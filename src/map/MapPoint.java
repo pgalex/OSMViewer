0b21ce04a713c9ccae6ad7eb055467b4c0a41f34
Alexander P. <pgalex@mail.ru>
2012-04-20 22:38:42 +0400

    point (one node) on a map

---
+++ src/map/MapPoint.java
@@ -0,0 +1,42 @@
+package map;
+
+/**
+ * Object on a map, consisting of one node
+ *
+ * @author pgalex
+ */
+public class MapPoint extends MapObject
+{
+	/**
+	 * Position of object on a map (spheric coords)
+	 */
+	private MapPosition position;
+
+	/**
+	 * Constructor
+	 *
+	 * @param pPosition position on a map (spheric coords)
+	 * @param pId global unique id from OpenStreetMap
+	 * @param pDefenitionTags defenition tags
+	 * @throws NullPointerException position is null 
+	 */
+	public MapPoint(MapPosition pPosition, long pId, DefenitionTags pDefenitionTags) throws NullPointerException
+	{
+		super(pId, pDefenitionTags);
+
+		if (pPosition == null)
+			throw new NullPointerException();
+
+		position = pPosition;
+	}
+
+	/**
+	 * Get position on a map
+	 *
+	 * @return position on a map
+	 */
+	public MapPosition getPosition()
+	{
+		return position;
+	}
+}
---
+++ test/mapTests/MapPointTest.java
@@ -0,0 +1,59 @@
+package mapTests;
+
+import map.MapPoint;
+import map.MapPosition;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author pgalex
+ */
+public class MapPointTest
+{
+	public MapPointTest()
+	{
+	}
+
+	/**
+	 * Testing constructor - normal work
+	 */
+	@Test
+	public void constructorNormalTest()
+	{
+		MapPoint testPoint = new MapPoint(new MapPosition(10, 11), 12, null);
+		assertEquals(10, testPoint.getPosition().getLatitude(), 0.01);
+		assertEquals(10, testPoint.getPosition().getLatitude(), 0.01);
+		assertEquals(12, testPoint.getId());
+	}
+	
+	/**
+	 * Testing constructor - null position
+	 */
+	@Test
+	public void constructorNullPositionTest()
+	{
+		try
+		{
+			MapPoint testPoint = new MapPoint(null, 12, null);
+			fail();
+		}
+		catch( NullPointerException ex )
+		{
+			// ok
+		}
+	}
+
+
+	@BeforeClass
+	public static void setUpClass() throws Exception
+	{
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception
+	{
+	}
+}
