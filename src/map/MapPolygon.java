a98d97719a74cc5bb0d3fd2e2f5363f5d382feff
Alexander P. <pgalex@mail.ru>
2012-05-02 12:56:11 +0400

    polygon on a map

---
+++ src/map/MapPolygon.java
@@ -0,0 +1,26 @@
+package map;
+
+import java.util.ArrayList;
+import map.exceptions.LinePointsIsIncorrectException;
+
+/**
+ * Closed line(way) on a map
+ *
+ * @author pgalex
+ */
+public class MapPolygon extends MapLine
+{
+	/**
+	 * Constructor
+	 *
+	 * @param pId global OpenStreetMap id of object
+	 * @param pDefenitionTags Tags, describes the polygon
+	 * @param pPoints points of polygon
+	 * @throws LinePointsIsIncorrectException polygon points array is null, empty or
+	 * contains null elements
+	 */
+	public MapPolygon(long pId, DefenitionTags pDefenitionTags, ArrayList<MapPosition> pPoints) throws LinePointsIsIncorrectException
+	{
+		super(pId, pDefenitionTags, pPoints);
+	}
+}
