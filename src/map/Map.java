852964c7ebab49fe6161b60f50dcaac4ebf88cb6
Alexander P. <pgalex@mail.ru>
2012-05-02 13:29:58 +0400

    Map interface

---
+++ src/map/Map.java
@@ -0,0 +1,15 @@
+package map;
+
+/**
+ * Interface of map - container of map objects
+ *
+ * @author pgalex
+ */
+public interface Map
+{
+	/**
+	 * Sort all objects by draw priority
+	 */
+	public abstract void sortObjectByDrawPriority();
+	
+}
