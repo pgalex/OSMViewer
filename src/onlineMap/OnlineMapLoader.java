c618d86f72ff7e88dda05ac290f28dff5e5a5f40
Alexander P. <pgalex@mail.ru>
2012-05-20 10:37:25 +0400

    loadToMap - map points

--- src/onlineMap/OnlineMapLoader.java
+++ src/onlineMap/OnlineMapLoader.java
@@ -1,14 +1,17 @@
 package onlineMap;
 
 import drawingStyle.StyleViewer;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import map.MapBounds;
+import java.util.ArrayList;
+import map.*;
 import map.exceptions.MapBoundsIsNullRuntimeException;
 import map.exceptions.MapIsNullRutimeException;
 import map.exceptions.StyleViewerIsNullException;
+import osmXml.OsmNode;
+import osmXml.OsmTag;
 
 /**
  * Creates connection to openstreetmap.org and loads map data and convertes it
@@ -51,22 +54,118 @@ public class OnlineMapLoader
 			throw new StyleViewerIsNullException();
 		if (pFillingMap == null)
 			throw new MapIsNullRutimeException();
-
+		
+		if (pLoadingSectorBounds.isZero())
+			return;
+		
 		try
 		{
-			URL openStreetMapURL = new URL("http://api.openstreetmap.org/api/0.6/map?bbox=35.7155,53.9239,35.7811,53.971");
+			URL openStreetMapURL = new URL("http://api.openstreetmap.org/api/0.6/map?bbox=35.7155,53.9239,35.7811,53.971");;
 			URLConnection openStreetMapConnection = openStreetMapURL.openConnection();
 			onlineParser.convert(openStreetMapConnection.getInputStream());
+			fillMapWithPoints(onlineParser.getParserNodes(), pFillingMap);
+		}
+		catch (MalformedURLException ex)
+		{
+			// connection error
+		}
+		catch (IOException ex)
+		{
+			// getting data error
+		}
+		catch (OutOfMemoryError ex)
+		{
+			// out of memory error
 		}
 		catch (Exception ex)
 		{
-			Logger.getLogger(OnlineMapLoader.class.getName()).log(Level.SEVERE, null, ex);
+			// parsing error
 		}
+	}
 
-		/*
-		 * Проверить границы загружаемого сектора Проверить заполняемую карту
-		 * Создать объект для чтения Обнулить парсер Передать в парсер объект для
-		 * чтения Заполнить карту данными из парсера
-		 */
+	/**
+	 * Create map points from osm nodes and add them to map
+	 *
+	 * @param pNodes array of nodes, readed from .osm
+	 * @param pFillingMap map, filling with map points, created by osm nodes
+	 */
+	private void fillMapWithPoints(ArrayList<OsmNode> pNodes, OnlineMap pFillingMap)
+	{
+		if (pFillingMap == null)
+			return;
+		if (pNodes == null)
+			return;
+		if (pNodes.isEmpty())
+			return;
+		
+		for (OsmNode currentNode : pNodes)
+		{
+			MapPoint newPoint = createMapPointByOsmNode(currentNode);
+			if (newPoint != null)
+				pFillingMap.addObject(newPoint);
+		}
+	}
+
+	/**
+	 * Creates map point by osm node
+	 *
+	 * @param pNode osm node
+	 * @return map point created by osm node
+	 */
+	private MapPoint createMapPointByOsmNode(OsmNode pNode)
+	{
+
+		if (pNode == null)
+			return null;
+		if (pNode.getTags() == null)
+			return null;
+		if (pNode.getTags().isEmpty())
+			return null;
+		
+		DefenitionTags creatingPointTags = createDefentionTagsByOsmTags(pNode.getTags());
+		if (creatingPointTags == null)
+			return null;
+		
+		MapPoint creatingPoint = new MapPoint(new MapPosition(pNode.getLatitude(), pNode.getLongitude()),
+						pNode.getId(), creatingPointTags);
+		
+		return creatingPoint;
+	}
+
+	/**
+	 * Create defenition tags by osm tags array
+	 *
+	 * @param pOsmTags array of osm tags
+	 * @return defenition tags created by osm tags array
+	 */
+	private DefenitionTags createDefentionTagsByOsmTags(ArrayList<OsmTag> pOsmTags)
+	{
+		if (pOsmTags == null)
+			return null;
+		
+		EditableDefenitionTags creatingTags = new EditableDefenitionTags();
+		for (int i = 0; i < pOsmTags.size(); i++)
+		{
+			MapTag newTag = createMapTagByOsmTag(pOsmTags.get(i));
+			if (newTag != null)
+				creatingTags.add(newTag);
+		}
+		return creatingTags;
+	}
+
+	/**
+	 * Create map tag by osm tag
+	 *
+	 * @param pOsmTag osm tag
+	 * @return map tag created by osm tag
+	 */
+	private MapTag createMapTagByOsmTag(OsmTag pOsmTag)
+	{
+		if (pOsmTag == null)
+			return null;
+		if (pOsmTag.getKey().isEmpty() || pOsmTag.getValue().isEmpty())
+			return null;
+		
+		return new MapTag(pOsmTag.getKey(), pOsmTag.getValue());
 	}
 }
