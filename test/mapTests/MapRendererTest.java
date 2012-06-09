ba5e4a6b72743e68d9f6d320e5b35328dc2871a4
Alexander P. <pgalex@mail.ru>
2012-06-09 17:43:51 +0400

    map renderer tests refactoring

--- test/mapTests/MapRendererTest.java
+++ test/mapTests/MapRendererTest.java
@@ -8,44 +8,74 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 /**
+ * MapRenderer class tests
  *
  * @author pgalex
  */
 public class MapRendererTest
 {
 	/**
-	 * Testing setBackground work
+	 * Testing initializing background color with default value in default
+	 * constructor
 	 */
 	@Test
-	public void setBackgroundTest()
+	public void initializingBackgroundColorTest()
 	{
 		MapRenderer testRenderer = new MapRenderer();
 		assertNotNull(testRenderer.getBackgroundColor());
+	}
 
-		//not null
+	/**
+	 * Test setting new background color
+	 */
+	@Test
+	public void setNewBackgroundColorTest()
+	{
+		MapRenderer testRenderer = new MapRenderer();
 		testRenderer.setBackgroundColor(Color.RED);
 		assertEquals(Color.RED, testRenderer.getBackgroundColor());
+	}
 
-		//null
+	/**
+	 * Test setting null background color
+	 */
+	@Test
+	public void setNullBackgroundColorTest()
+	{
+		MapRenderer testRenderer = new MapRenderer();
 		testRenderer.setBackgroundColor(Color.GREEN);
 		testRenderer.setBackgroundColor(null);
 		assertEquals(Color.GREEN, testRenderer.getBackgroundColor());
 	}
 
 	/**
-	 * Testing setDrawingArea work
+	 * Testing initializing drawing area with default value in default constructor
 	 */
 	@Test
-	public void setDrawingAreaTest()
+	public void initializingDrawingAreaTest()
 	{
 		MapRenderer testRenderer = new MapRenderer();
 		assertNotNull(testRenderer.getDrawingArea());
+	}
 
-		//not null
+	/**
+	 * Test setting new value of drawing area
+	 */
+	@Test
+	public void setNewDrawingAreaTest()
+	{
+		MapRenderer testRenderer = new MapRenderer();
 		testRenderer.setDrawingArea(new Rectangle(10, 10, 20, 20));
 		assertEquals(new Rectangle(10, 10, 20, 20), testRenderer.getDrawingArea());
+	}
 
-		//null
+	/**
+	 * Test setting null value of drawing area
+	 */
+	@Test
+	public void setNullDrawingAreaTest()
+	{
+		MapRenderer testRenderer = new MapRenderer();
 		testRenderer.setDrawingArea(new Rectangle(20, 20, 30, 30));
 		testRenderer.setDrawingArea(null);
 		assertEquals(new Rectangle(20, 20, 30, 30), testRenderer.getDrawingArea());
