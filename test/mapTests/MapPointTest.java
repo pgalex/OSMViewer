7a93bdde323e4310ded6c0816c50256fc9564338
Alexander P. <pgalex@mail.ru>
2012-06-09 17:54:16 +0400

    map point tests refactoring

--- test/mapTests/MapPointTest.java
+++ test/mapTests/MapPointTest.java
@@ -3,59 +3,44 @@ package mapTests;
 import map.MapPoint;
 import map.MapPosition;
 import map.exceptions.PointPositionIsNullException;
-import org.junit.AfterClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
+ * MapPoint class tests
  *
  * @author pgalex
  */
 public class MapPointTest
 {
-	public MapPointTest()
-	{
-	}
-
 	/**
-	 * Testing constructor - normal work
+	 * Testing creating point with null style index
 	 */
 	@Test
-	public void constructorNormalTest()
+	public void creatingWithNullStyleIndexTest()
 	{
 		MapPoint testPoint = new MapPoint(new MapPosition(10, 11), 12, null);
 		assertEquals(10, testPoint.getPosition().getLatitude(), 0.01);
 		assertEquals(10, testPoint.getPosition().getLatitude(), 0.01);
 		assertEquals(12, testPoint.getId());
+		assertNull(testPoint.getStyleIndex());
 	}
-	
+
 	/**
-	 * Testing constructor - null position
+	 * Testing creating with null position
 	 */
 	@Test
-	public void constructorNullPositionTest()
+	public void creatingWithNullPositionTest()
 	{
 		try
 		{
 			MapPoint testPoint = new MapPoint(null, 12, null);
 			fail();
 		}
-		catch( PointPositionIsNullException ex )
+		catch (PointPositionIsNullException ex)
 		{
 			assertEquals(12, ex.getCreatedObjectId());
 			assertNull(ex.getCreatedObjectTags());
 		}
-	}
-
-
-	@BeforeClass
-	public static void setUpClass() throws Exception
-	{
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception
-	{
 	}
 }
