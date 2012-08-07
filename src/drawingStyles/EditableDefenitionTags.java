140e52753c8def47812036f32d2c50d0169b339f
Alexander P. <pgalex@mail.ru>
2012-08-07 20:34:05 +0400

    editable defenition tags refactoring

--- src/drawingStyles/EditableDefenitionTags.java
+++ src/drawingStyles/EditableDefenitionTags.java
@@ -1,18 +1,17 @@
 package drawingStyles;
 
-import drawingStyles.DefenitionTags;
 import map.exceptions.TagIndexOutOfBoundsException;
 import map.exceptions.TagIsNullException;
 
 /**
- * Defenition tags with "edit" methods
+ * Defenition tags that can be changed
  *
  * @author pgalex
  */
 public class EditableDefenitionTags extends DefenitionTags
 {
 	/**
-	 * Default construutor
+	 * Create empty editable tags
 	 */
 	public EditableDefenitionTags()
 	{
@@ -22,18 +21,21 @@ public class EditableDefenitionTags extends DefenitionTags
 	/**
 	 * Add tag
 	 *
-	 * @param pTag tag
-	 * @throws TagIsNullException tag is null
+	 * @param pTag adding tag
+	 * @throws TagIsNullException adding tag is null
 	 */
 	public void add(MapTag pTag) throws TagIsNullException
 	{
 		if (pTag == null)
+		{
 			throw new TagIsNullException(this);
+		}
+
 		tags.add(pTag);
 	}
 
 	/**
-	 * Clear tags array
+	 * Remove all tags
 	 */
 	public void clear()
 	{
@@ -41,15 +43,18 @@ public class EditableDefenitionTags extends DefenitionTags
 	}
 
 	/**
-	 * Remove elemet with index
+	 * Remove tag with index
 	 *
-	 * @param pIndex index of element in tags array
-	 * @throws TagIndexOutOfBoundsException index out of bounds
+	 * @param pIndex index of tag
+	 * @throws TagIndexOutOfBoundsException index of deleting tag is out of bounds
 	 */
 	public void remove(int pIndex) throws TagIndexOutOfBoundsException
 	{
 		if (pIndex < 0 || pIndex >= tags.size())
+		{
 			throw new TagIndexOutOfBoundsException(this, pIndex, 0, tags.size());
+		}
+
 		tags.remove(pIndex);
 	}
 }
