552a2ad24955a6f5cd053c44fb7369ab4e24fb15
Alexander P. <pgalex@mail.ru>
2013-03-30 13:16:41 +0400

    fetching map objects from database

--- src/com/osmviewer/sqliteMapDataSource/SQLiteDataBaseMapDataSource.java
+++ src/com/osmviewer/sqliteMapDataSource/SQLiteDataBaseMapDataSource.java
@@ -18,8 +18,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Map data source by SQLite database
