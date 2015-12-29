65a1b261bd30fc6d0bce9d0b85d1a8e968c16de0
preobrazhentsev <preobrazhentsev@PREOBRAZHENTSEV.klmn.ascon.local>
2015-12-29 15:14:23 +0300

    test finding draw priority in xml

---
+++ OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlDrawPriorityContainerTest.java
@@ -0,0 +1,16 @@
+package com.osmviewer.xmlDrawSettingsTests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author preobrazhentsev
+ */
+public class XmlDrawPriorityContainerTest
+{
+	@Test
+	public void hello()
+	{
+	}
+}
