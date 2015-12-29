54cbb855aeee92b24c240563310b4ecd509a0c9e
preobrazhentsev <preobrazhentsev@PREOBRAZHENTSEV.klmn.ascon.local>
2015-12-29 15:16:46 +0300

    test finding draw priority in xml

--- OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlDrawPriorityContainerTest.java
+++ OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlDrawPriorityContainerTest.java
@@ -1,5 +1,9 @@
 package com.osmviewer.xmlDrawSettingsTests;
 
+import com.osmviewer.xmlDrawSettings.XmlDrawPriorityContainer;
+import com.osmviewer.xmlDrawSettings.exceptions.ParsingXmlErrorException;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -10,7 +14,20 @@ import static org.junit.Assert.*;
 public class XmlDrawPriorityContainerTest
 {
 	@Test
-	public void hello()
+	public void correctParsingXmlDrawPriority() throws ParsingXmlErrorException
 	{
+		final String xmlDrawPriority = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+						+ "<drawPriority>\n"
+						+ "	<mapObject id=\"firstMapObject\"/>\n"
+						+ "	<mapObject id=\"secondMapObject\"/>\n"
+						+ "</drawPriority>";
+
+		XmlDrawPriorityContainer xmlDrawPriorityContainer = new XmlDrawPriorityContainer();
+		xmlDrawPriorityContainer.readFromStream(new ByteArrayInputStream(xmlDrawPriority.getBytes(StandardCharsets.UTF_8)));
+
+		assertEquals(new Integer(0), xmlDrawPriorityContainer.getDrawPriorityOf("firstMapObject"));
+		assertEquals(new Integer(1), xmlDrawPriorityContainer.getDrawPriorityOf("secondMapObject"));
 	}
+
+	// finding draw priority of unexists object
 }
--- OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlMapObjectsIdFinderTest.java
+++ OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlMapObjectsIdFinderTest.java
@@ -1,33 +1,10 @@
 package com.osmviewer.xmlDrawSettingsTests;
 
-import com.osmviewer.xmlDrawSettings.XmlDrawPriorityContainer;
-import com.osmviewer.xmlDrawSettings.exceptions.ParsingXmlErrorException;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 /**
  *
  * @author preobrazhentsev
  */
 public class XmlMapObjectsIdFinderTest
 {
-	@Test
-	public void correctParsingXmlDrawPriority() throws ParsingXmlErrorException
-	{
-		final String xmlDrawPriority = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-						+ "<drawPriority>\n"
-						+ "	<mapObject id=\"firstMapObject\"/>\n"
-						+ "	<mapObject id=\"secondMapObject\"/>\n"
-						+ "</drawPriority>";
 
-		XmlDrawPriorityContainer xmlDrawPriorityContainer = new XmlDrawPriorityContainer();
-		xmlDrawPriorityContainer.readFromStream(new ByteArrayInputStream(xmlDrawPriority.getBytes(StandardCharsets.UTF_8)));
-		
-		assertEquals(new Integer(0), xmlDrawPriorityContainer.getDrawPriorityOf("firstMapObject"));
-		assertEquals(new Integer(1), xmlDrawPriorityContainer.getDrawPriorityOf("secondMapObject"));
-	}
-	
-	// finding draw priority of unexists object
 }
