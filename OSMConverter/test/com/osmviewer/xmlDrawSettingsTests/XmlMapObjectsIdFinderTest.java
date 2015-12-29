52632a1364af0d047bf5e56a8ae4eb8ab1c07b14
preobrazhentsev <preobrazhentsev@PREOBRAZHENTSEV.klmn.ascon.local>
2015-12-29 15:03:40 +0300

    xml draw settings tests

---
+++ OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlMapObjectsIdFinderTest.java
@@ -0,0 +1,16 @@
+package com.osmviewer.xmlDrawSettingsTests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author preobrazhentsev
+ */
+public class XmlMapObjectsIdFinderTest
+{
+	@Test
+	public void hello()
+	{
+	}
+}
