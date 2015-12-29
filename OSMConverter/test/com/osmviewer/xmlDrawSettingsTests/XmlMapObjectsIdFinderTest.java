3c697b14dce1aeeb06cd8d7922cca0b878beadc6
preobrazhentsev <preobrazhentsev@PREOBRAZHENTSEV.klmn.ascon.local>
2015-12-29 15:21:37 +0300

    test finding draw priority in xml

--- OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlMapObjectsIdFinderTest.java
+++ OSMConverter/test/com/osmviewer/xmlDrawSettingsTests/XmlMapObjectsIdFinderTest.java
@@ -1,10 +1,17 @@
 package com.osmviewer.xmlDrawSettingsTests;
 
+import com.osmviewer.xmlDrawSettings.exceptions.ParsingXmlErrorException;
+import org.junit.Test;
+
 /**
  *
  * @author preobrazhentsev
  */
 public class XmlMapObjectsIdFinderTest
 {
+	@Test
+	public void hello()
+	{
 
+	}
 }
