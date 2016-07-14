bfcd3e8578119004f346de1076e76c516da0f666
preobrazhentsev <preobrazhentsev@ascon.ru>
2016-07-14 12:07:29 +0300

    simplifying map objects draw settings

--- OSMConverter/src/com/osmviewer/forms/ConvertOsmXmlToSQLiteDialog.java
+++ OSMConverter/src/com/osmviewer/forms/ConvertOsmXmlToSQLiteDialog.java
@@ -106,7 +106,7 @@ public class ConvertOsmXmlToSQLiteDialog extends javax.swing.JDialog
 		 * Set the Nimbus look and feel
 		 */
 		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /*
+		/*
 		 * If Nimbus (introduced in Java SE 6) is not available, stay with the
 		 * default look and feel. For details see
 		 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
